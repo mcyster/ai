7a3cbf7b015664cc751444988c7434cacb9b729d
Mark Cyster <mcyster@extole.com>
2024-05-12 17:21:10 -0700

    Move to jvm-openai library

--- assistant/src/main/java/com/cyster/assistant/impl/advisor/AdvisorServiceImpl.java
+++ assistant/src/main/java/com/cyster/assistant/impl/advisor/AdvisorServiceImpl.java
@@ -1,8 +1,13 @@
 package com.cyster.assistant.impl.advisor;
 
 
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+import com.cyster.assistant.impl.conversation.TooledChatConversation;
 import com.cyster.assistant.service.advisor.AdvisorBuilder;
 import com.cyster.assistant.service.advisor.AdvisorService;
+
 import io.github.stefanbratanov.jvm.openai.OpenAI;
 
 // https://platform.openai.com/docs/assistants/overview
@@ -16,8 +21,8 @@ public class AdvisorServiceImpl implements AdvisorService {
 
     private final OpenAI openAi;
     
-    public AdvisorServiceImpl(OpenAI openAi) {
-        this.openAi = openAi;
+    public AdvisorServiceImpl(String openAiKey) {
+        this.openAi = createOpenAiService(openAiKey, true);
     }
     
     public <C> AdvisorBuilder<C> getOrCreateAdvisor(String name) {
@@ -25,6 +30,24 @@ public class AdvisorServiceImpl implements AdvisorService {
         return new AssistantAdvisorImpl.Builder<C>(this.openAi, name);    
     }
      
+    // TBD is this an advisor ??? 
+    public TooledChatConversation createTooledChatConversation() {
+        return new TooledChatConversation(this.openAi);
+    }
+    
+    
+    private static OpenAI createOpenAiService(String openApiKey, Boolean debug) {
+        HttpClient httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(20))
+            .build();
+        
+        // TODO support logging request / response   
+     
+        return OpenAI.newBuilder(System.getenv("OPENAI_API_KEY"))
+                .httpClient(httpClient)
+                .build();             
+    }
+    
     
  
 }
