df71340f874e476b27daa0db210469fd0975d222
Mark Cyster <mcyster@gmail.com>
2024-07-07 18:20:30 -0700

    extole-report-view to not use prompts

--- sage-app/src/main/java/com/cyster/app/sage/SageAppConfig.java
+++ sage-app/src/main/java/com/cyster/app/sage/SageAppConfig.java
@@ -31,6 +31,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
--- sites/managed/extole-report-viewer/data.js
+++ sites/managed/extole-report-viewer/data.js
@@ -1,101 +1,19 @@
 
-function getExtoleToken() {
-    return new Promise((resolve, reject) => {
-        let token = localStorage.getItem('extole_token');
-        
-        if (!token) {
-            token = prompt("Please enter your Extole token:");
-            if (token) {
-                localStorage.setItem('extole_token', token);
-            } else {
-                return reject(new Error('Authorization token not provided'));
+async function getData(reportId, extoleToken) {
+    try {
+        const response = await fetch(`https://api.extole.io/v4/tokens/${extoleToken}/download.json?report_id={reportId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${extoleToken}`
             }
-        }
-
-        validateToken(token)
-            .then(isValid => {
-                if (isValid) {
-                    resolve(token);
-                } else {
-                    localStorage.removeItem('extole_token');
-                    reject(new Error('Invalid token. Please try again.'));
-                }
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
-}
-
-function validateToken(token) {
-    const endpoint = `https://api.extole.io/v4/tokens/${token}`;
-
-    return fetch(endpoint, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    })
-    .then(response => {
-        if (response.status === 403) {
-            return false;
-        }
-        return response.json().then(data => {
-            console.log('Token is valid. Details:', data);
-            return true;
         });
-    })
-    .catch(error => {
-        console.error('Error validating token:', error);
-        return false;
-    });
-}
-
-
-function getReportId() {
-    const urlParams = new URLSearchParams(window.location.search);
-    reportId = urlParams.get('report_id');
-    if (!reportId) {
-        alert("Error: No report_id parameter");
-        return Promise.reject('No report_id parameter');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
     }
-    return reportId;
 }
-
-function getData() {
-    return new Promise((resolve, reject) => {
-        const reportId = getReportId();
-        const url = `https://api.extole.io/v4/reports/${reportId}/download.json`;
-
-        getExtoleToken()
-            .then(token => {
-                if (!token) {
-                    alert("Error: No token");
-                    return reject('Authorization token not found');
-                }
-
-                return fetch(url, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                });
-            })
-            .then(response => {
-                if (!response.ok) {
-                    alert("Error: unable to fetch " + url);
-                    return reject(new Error('Error fetching "' + url + '": ' + response.statusText));
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log("got data", data);
-                resolve(data);
-            })
-            .catch(error => {
-                alert("Error: problem fetching " + url);
-                reject(error);
-            });
-    });
-}
-
