9e8cb25f24fb866b48d9c0e427ddf5fba4b3eebd
Mark Cyster <mcyster@gmail.com>
2024-07-08 19:51:36 -0700

    Fix jira-app config to not define a mapper

--- jira-app/src/main/java/com/extole/app/jira/JiraAppConfig.java
+++ jira-app/src/main/java/com/extole/app/jira/JiraAppConfig.java
@@ -73,22 +73,6 @@ public class JiraAppConfig implements WebMvcConfigurer {
         return new WebsiteServiceImpl(applicationUri.resolve("/sites"), sites); 
     }
     
-    
-    @Bean
-    public ObjectMapper objectMapper() {
-        return Jackson2ObjectMapperBuilder
-        .json()
-        .featuresToEnable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-        .build();
-    }
-    
-    @Bean 
-    public Jackson2ObjectMapperBuilder objectMapperBuilder(){
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.failOnUnknownProperties(true);
-        return builder;
-    }
-    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String resourcePath = "file:" + sites.toAbsolutePath().toString() + "/";
