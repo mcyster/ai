eb3508ddd0a822e7a1418fe4d9de2adf248e3f8f
Mark Cyster <mcyster@gmail.com>
2024-07-18 10:29:53 -0700

    Add ExtoleRunbookSelectorScenario to help debug the selector prompt

--- extole-weave/src/main/java/com/extole/weave/scenarios/runbooks/ExtoleRunbookSelectorScenario.java
+++ extole-weave/src/main/java/com/extole/weave/scenarios/runbooks/ExtoleRunbookSelectorScenario.java
@@ -11,34 +11,30 @@ import com.cyster.ai.weave.service.AssistantScenarioBuilder;
 import com.cyster.ai.weave.service.SearchTool;
 import com.cyster.ai.weave.service.Tool;
 import com.cyster.ai.weave.service.scenario.Scenario;
-import com.extole.weave.scenarios.support.tools.jira.SupportTicketGetTool;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.extole.weave.scenarios.runbooks.ExtoleSupportTicketRunbookSelectorScenario.Parameters;
 
 @Component
-public class ExtoleSupportTicketRunbookSelectorScenario implements Scenario<Parameters, Void> {
-    public final String NAME = "extoleSupportTicketRunbookSelector";
-    private final String DESCRIPTION = "Find the best Runbook for the specified ticket";
+public class ExtoleRunbookSelectorScenario implements Scenario<Void, Void> {
+    public final String NAME = "extoleRunbookSelector";
+    private final String DESCRIPTION = "Find the best Runbook given a set of keywords (intended for testing)";
 
     private AiWeaveService aiWeaveService;
-    private Optional<Scenario<Parameters, Void>> scenario = Optional.empty();
+    private Optional<Scenario<Void, Void>> scenario = Optional.empty();
     private List<Tool<?, Void>> tools = new ArrayList<>();
     private String defaultRunbookName;
     private SearchTool<Void> searchTool;
     
-    public ExtoleSupportTicketRunbookSelectorScenario(AiWeaveService aiWeaveService, ExtoleRunbookToolFactory runbookToolFactory,
-            SupportTicketGetTool ticketGetTool,
+    public ExtoleRunbookSelectorScenario(AiWeaveService aiWeaveService, ExtoleRunbookToolFactory runbookToolFactory,
             ExtoleRunbookDefault defaultRunbook) {
         this.aiWeaveService = aiWeaveService;
         this.tools.add(runbookToolFactory.getRunbookSearchTool());
-        this.tools.add(ticketGetTool);
         this.defaultRunbookName = defaultRunbook.getName();
         this.searchTool = runbookToolFactory.getRunbookSearchTool();
     }
 
     @Override
     public String getName() {
-        return NAME;
+        return this.getClass().getSimpleName().replace("Scenario", "");
     }
     @Override
     public String getDescription() {
@@ -46,8 +42,8 @@ public class ExtoleSupportTicketRunbookSelectorScenario implements Scenario<Para
     }
     
     @Override
-    public Class<Parameters> getParameterClass() {
-        return Parameters.class;
+    public Class<Void> getParameterClass() {
+        return Void.class;
     }
 
     @Override
@@ -56,28 +52,16 @@ public class ExtoleSupportTicketRunbookSelectorScenario implements Scenario<Para
     }
 
     @Override
-    public ConversationBuilder createConversationBuilder(Parameters parameters, Void context) {         
-        if (parameters == null || parameters.ticketNumber() == null || parameters.ticketNumber().isBlank()) {
-            throw new IllegalArgumentException("No ticketNumber specified");
-        }
-
-        if (!searchTool.isReady()) {
-            // Still seem to have to wait a bit sometimes, even when its ready here
-            System.out.println("!!!!!!!!!! search tool - vector store not ready !!!");
-        }
-        
-        return getScenario().createConversationBuilder(parameters, context)
-            .addMessage("Ticket: " + parameters.ticketNumber());
+    public ConversationBuilder createConversationBuilder(Void parameters, Void context) {         
+        return getScenario().createConversationBuilder(parameters, context);
     }
     
-    private Scenario<Parameters, Void> getScenario() {
+    private Scenario<Void, Void> getScenario() {
         if (this.scenario.isEmpty()) {
             String instructionsTemplate = """
-You are an Extole Support Team member handling an incoming ticket. Your task is to identify the most appropriate Runbook for resolving the ticket's issue.
+You are an Extole Support Team member handling an incoming ticket. Your task is to identify the most appropriate Runbook from a prompt.
 
-Ticket Information: Use the ticketGet to get the specified ticket
-
-Construct a query string from the ticket description.
+Construct a query string from the prompt.
 - Remove any personally identifiable information (PII).
 - Remove any company names.
 - Remove any URLs.
@@ -109,7 +93,7 @@ Provide your answer in JSON format as describe by this schema:
 
             System.out.println("!!!!!!!! extole support ticket instructions: " + instructions);
             
-            AssistantScenarioBuilder<Parameters, Void> builder = this.aiWeaveService.getOrCreateAssistantScenario(NAME);
+            AssistantScenarioBuilder<Void, Void> builder = this.aiWeaveService.getOrCreateAssistantScenario(NAME);
             builder.setInstructions(instructions);
 
             for(var tool: tools) {
@@ -121,12 +105,7 @@ Provide your answer in JSON format as describe by this schema:
         return this.scenario.get();
     }
     
-    public record Parameters(
-        @JsonProperty(required = true) String ticketNumber
-    ) {}
-
     public record Response (
-        @JsonProperty(required = true) String ticketNumber,
         @JsonProperty(required = true) String runbookName,
         @JsonProperty(required = false) String query,
         @JsonProperty(required = false) String[] searchResults 
