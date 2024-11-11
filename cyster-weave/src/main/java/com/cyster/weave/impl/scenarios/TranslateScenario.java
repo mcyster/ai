6eccad5a37c1005aaad2d2f0bd4dbc328c674b65
Mark Cyster <mcyster@extole.com>
2024-11-11 14:53:00 -0800

    Use StringTemplate everywhere

--- cyster-weave/src/main/java/com/cyster/weave/impl/scenarios/TranslateScenario.java
+++ cyster-weave/src/main/java/com/cyster/weave/impl/scenarios/TranslateScenario.java
@@ -1,17 +1,12 @@
 package com.cyster.weave.impl.scenarios;
 
-import java.io.StringReader;
-import java.io.StringWriter;
-
 import org.springframework.stereotype.Component;
 
 import com.cyster.ai.weave.service.scenario.Scenario;
+import com.cyster.template.StringTemplate;
 import com.cyster.weave.impl.scenarios.TranslateScenario.Parameters;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
 
 @Component
 public class TranslateScenario implements Scenario<Parameters, Void> {
@@ -40,22 +35,16 @@ public class TranslateScenario implements Scenario<Parameters, Void> {
     public Class<Void> getContextClass() {
         return Void.class;
     }
-    
+
     @Override
     public ConversationBuilder createConversationBuilder(Parameters parameters, Void context) {
-        String systemPrompt = "Please translate messages from {{language}} to {{target_language}}.";
+        String instructionsTemplate = "Please translate messages from {{language}} to {{target_language}}.";
 
-        MustacheFactory mostacheFactory = new DefaultMustacheFactory();
-        Mustache mustache = mostacheFactory.compile(new StringReader(systemPrompt), "system_prompt");
-        var messageWriter = new StringWriter();
-        mustache.execute(messageWriter, parameters);
-        messageWriter.flush();
-        var instructions = messageWriter.toString();
+        String instructions = new StringTemplate(instructionsTemplate).render(parameters);
 
-        return this.chatScenario.createConversationBuilder(null, context)
-            .setOverrideInstructions(instructions);
+        return this.chatScenario.createConversationBuilder(null, context).setOverrideInstructions(instructions);
     }
-    
+
     static class Parameters {
         @JsonPropertyDescription("language to translate from, ISO 639-1, defaults to en")
         @JsonProperty(required = false)
