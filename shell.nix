a08ec9e42dc0f73e5ec66c9fd4e45e3b84cec0cf
Mark Cyster <mcyster@extole.com>
2025-02-02 10:41:11 -0800

    Backport .ai-rc from ai-sage

--- shell.nix
+++ shell.nix
@@ -18,6 +18,9 @@ in
 
     shellHook = ''
       export AI_HOME=${builtins.getEnv "PWD"}
+      if [ -f $HOME/.ai-rc ]; then
+        source $HOME/.ai-rc
+      fi
       export SPRING_AI_OPENAI_API_KEY=$OPENAI_API_KEY
       export LANG=en_US.UTF-8
     '';
