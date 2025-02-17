15f1038ee5414ea4aae2711626a4a8b108d71c21
Mark Cyster <mcyster@extole.com>
2025-02-16 16:25:31 -0800

    endpoint to set app token

---
+++ jira-app/src/main/java/com/extole/app/jira/root/TokenController.java
@@ -0,0 +1,51 @@
+package com.extole.app.jira.root;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+public class TokenController {
+
+    private final boolean isSecure;
+
+    public TokenController(@Value("${app.url}") String appUrl) {
+
+        if (appUrl.startsWith("https")) {
+            this.isSecure = true;
+        } else {
+            this.isSecure = false;
+        }
+
+    }
+
+    @GetMapping("/app-token")
+    @ResponseStatus(HttpStatus.FOUND)
+    public void setTokenFromHeaderAndRedirect(@RequestParam String url, HttpServletRequest request,
+            HttpServletResponse response) {
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            Cookie cookie = new Cookie("id_token", token);
+            cookie.setPath("/");
+            cookie.setHttpOnly(false);
+            if (isSecure) {
+                cookie.setSecure(true);
+            }
+            cookie.setMaxAge(3600);
+
+            response.addCookie(cookie);
+        }
+
+        response.setHeader("Location", url);
+    }
+}
